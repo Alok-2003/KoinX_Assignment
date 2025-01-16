3a8e37612cc996cf7ddd4d200b673a3380f818c3
Alok-2003 <alok953280@gmail.com>
2025-01-16 11:55:56 +0530

    test 1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,12 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "warn", // Change to "warn" or "off" to reduce the strictness
-        {
-          args: "none", // Ignore unused function arguments
-          vars: "all",  // Check all variables but allow exceptions
-          varsIgnorePattern: "^_", // Ignore variables prefixed with "_"
-          argsIgnorePattern: "^_", // Ignore arguments prefixed with "_"
-        },
-      ],
-    },
-  },
 ];
-
 export default eslintConfig;
